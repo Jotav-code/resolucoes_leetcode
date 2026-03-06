5d40839f3883c50bec478eb4466a16acd3d266cc
João Vitor Soares Bispo <joaovitor646260@Gmail.com>
2026-03-05 21:13:53 -0300

    Update numero_palidromo.c
    resolvendo bug, troquei a lógica de separação do número

--- numero_palidromo.c
+++ numero_palidromo.c
@@ -1,31 +1,24 @@
-#include<stdio.h>
-#include<math.h>
-
-int main() {
-    float numero_copy = 999;
+bool isPalindrome(int x){
+    int numero_original = x ;
+    int numero_copy = x;
     int numero_invertido[100];
     int i = 0;
     while(numero_copy > 0){
-        numero_copy = numero_copy / 10;
-        // printf("%f aqui", numero_copy);
-        float quebra_numero = numero_copy;
-        // printf("%f eu \n", quebra_numero);
-        numero_invertido[i] =  ((quebra_numero - floor(numero_copy)) * 10) + 0.00001; 
-        numero_copy = trunc(numero_copy);
+        numero_invertido[i] = numero_copy % 10 ; 
+        numero_copy = numero_copy /10;
         i++;
     }
-    int valor_final = 0;
+    double valor_final = 0;
     int k = 0;
     for(int j = i; j > 0; j--){
         valor_final += numero_invertido[k++] * pow(10 , j);
     }
     valor_final = valor_final/10;
-    printf("%d",valor_final );
+    // printf("%d",valor_final );
 
     for(int j = 0; j < i; j++ ){
-        printf("invertido %d", numero_invertido[j]); 
+        printf(" %d ", numero_invertido[j]); 
     }
-    
     if(valor_final == numero_original){
         return true;
     }else{
